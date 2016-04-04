f891a9fba66bcc3fcbeffcd91708672a31f2a1e7
Bartosz Krol <adrilan.evenwood@gmail.com>
2016-04-04 23:26:08 +0200

    Create demo colaborative whiteboard

---
+++ index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({port: 3000});
+
+server.register(require('inert'), (err) => {
+  if (err) {
+    throw err;
+  }
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+      reply.file('./public/index.html');
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/board',
+    handler: function (request, reply) {
+      reply.file('./public/board.html');
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/bower_components/{param*}',
+    handler: {
+      directory: {
+        path: 'bower_components'
+      }
+    }
+  });
+});
+
+server.start((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Server running at:', server.info.uri);
+});
+
+var io = require('socket.io')(server.listener);
+io.on('connection', function (socket) {
+  socket.on('board', function (msg) {
+    socket.broadcast.emit('board', msg);
+  });
+});
